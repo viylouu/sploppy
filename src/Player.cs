08b86d9ddd52b54c381ed7195fc05503d76dd38e
viylouu <honeefunnee@gmail.com>
2024-09-21 18:48:25 -0600

    added back graphics

--- src/Player.cs
+++ src/Player.cs
@@ -1,15 +1,14 @@
-﻿partial class sploppy
-{
-    public class Player
-    {
-        const int gravity = 8;
+﻿using System;
+
+partial class sploppy {
+    public class Player {
+        const int gravity = 48;
         const int Drag = 2;
 
         static Vector2 Ppos = new Vector2(100, 20);
         static Vector2 Pvel = new Vector2(0, 0);
 
-        public static void Updateplayer()
-        {
+        public static void Updateplayer() {
             //Update Position
             Ppos += Pvel * Time.DeltaTime;
 
@@ -17,14 +16,18 @@
             Pvel.Y += gravity * Time.DeltaTime;
 
             //Input
-            if (Keyboard.IsKeyPressed(Key.E)) Pvel.Y = -16;
+            if (Keyboard.IsKeyPressed(Key.E)) Pvel.Y = -64;
         }
 
-        public static void Drawplayer(ICanvas canvas)
-        {
+        public static void Drawplayer(ICanvas canvas) {
             // Drawing the Player
-            canvas.Fill(Color.Green);
-            canvas.DrawRect(Ppos,new Vector2(8,6),Alignment.Center);
+            canvas.DrawTexture(sploppertex, Ppos ,Alignment.Center);
+
+            canvas.Translate(Ppos);
+            canvas.Rotate(atan2(Vector2.Normalize(Mouse.Position-Ppos)));
+            canvas.Translate(8,0);
+            canvas.DrawTexture(guntex, Vector2.Zero, new Vector2(16, 8 * (Mouse.Position.X<Ppos.X?-1:1)), Alignment.CenterLeft);
+            canvas.ResetState();
         }
     }
 }
--- src/renderer.cs
+++ src/renderer.cs
@@ -11,7 +11,9 @@
 
         c.DrawTexture(bgtex);
 
-        c.DrawTexture(cloudstex, -Time.TotalTime*bgscrollspeed%240, 0, Alignment.TopLeft);
-        c.DrawTexture(cloudstex, -Time.TotalTime*bgscrollspeed%240+240, 0, Alignment.TopLeft);
+        c.DrawTexture(cloudstex, -Time.TotalTime * bgscrollspeed % 240, 0, Alignment.TopLeft);
+        c.DrawTexture(cloudstex, -Time.TotalTime * bgscrollspeed % 240 + 240, 0, Alignment.TopLeft);
+
+        Player.Drawplayer(c);
     }
 }
